d91e3c91b02f53b0c0064f5d64ddaa56b182169f
Passion-Logan <wangdadamailbox@163.com>
2018-11-26 23:39:41 +0800

    添加增加删除功能

--- helloworld/src/Inputout.js
+++ helloworld/src/Inputout.js
@@ -6,28 +6,41 @@ class Inputout extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      list : [
-        'learn react',
-        'learn englist'
-      ]
+      list : [],
+      inputValue : ''
     }
   }
 
   handleBenClick() {
-    alert('click');
+    this.setState({
+      list: [...this.state.list, this.state.inputValue],
+      inputValue : ''
+    })
+  }
+
+  handleInputChange(e) {
+    this.setState({
+      inputValue : e.target.value
+    })
+  }
+
+  handleItemClick(index) {
+    const list = [...this.state.list];
+    list.splice(index, 1);
+    this.setState({list})
   }
   
   render() {
     return (
       <div>
         <div>
-          <input />
-          <button onClick={this.handleBenClick}>add</button>
+          <input value = {this.state.inputValue} onChange={this.handleInputChange.bind(this)} />
+          <button onClick={this.handleBenClick.bind(this)}>add</button>
         </div>
         <ul>
           {
-            this.state.list.map((item) => {
-              return <li>{item}</li>
+            this.state.list.map((item, index) => {
+              return <li key={index} onClick={this.handleItemClick.bind(this, index)}>{item}</li>
             })
           }
         </ul>
