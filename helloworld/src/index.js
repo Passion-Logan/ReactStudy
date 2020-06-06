2abefd89997bc19e4d6695e500f70943b4bf8792
Passion-Logan <wangdadamailbox@163.com>
2020-06-06 18:25:56 +0800

    添加测试例子

--- helloworld/src/index.js
+++ helloworld/src/index.js
@@ -5,3 +5,11 @@ import ReactDOM from 'react-dom'
 import Inputout from './Inputout'
 
 ReactDOM.render(<Inputout />, document.getElementById('root'))
+
+// function Welcome(props) {
+//   return <h1>hello, {props.name}</h1>
+// }
+
+// const element = <Welcome name=" world" />
+
+// ReactDOM.render(element, document.getElementById('root'))
