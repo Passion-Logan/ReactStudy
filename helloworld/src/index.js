890c49f15cc4058e0de82d2b02a752bbfa00085c
Passion-Logan <wangdadamailbox@163.com>
2020-06-06 18:16:01 +0800

    添加修改

--- helloworld/src/index.js
+++ helloworld/src/index.js
@@ -1,11 +1,7 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React from 'react'
+import ReactDOM from 'react-dom'
 
 //大写开头的都是组件
-import Inputout from './Inputout';
+import Inputout from './Inputout'
 
-ReactDOM.render( < Inputout / > , document.getElementById('root'));
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: http://bit.ly/CRA-PWA
+ReactDOM.render(<Inputout />, document.getElementById('root'))
