c92902bae53111182b0c84312749a18bdd761b9d
Passion-Logan <wangdadamailbox@163.com>
2020-06-06 16:41:56 +0800

    格式化代码

--- helloworld/src/Inputout.js
+++ helloworld/src/Inputout.js
@@ -1,68 +1,68 @@
-import React, { Component } from 'react';
-import InputItem from './InputItem';
+import React, { Component } from 'react'
+import InputItem from './InputItem'
 
 // 定义一个React组件
 class Inputout extends Component {
-
   constructor(props) {
-    super(props);
+    super(props)
     this.state = {
-      list : [],
-      inputValue : ''
+      list: [],
+      inputValue: '',
     }
 
-    this.handleBenClick = this.handleBenClick.bind(this);
-    this.handleInputChange = this.handleInputChange.bind(this);
-    this.handleDelete = this.handleDelete.bind(this);
+    this.handleBenClick = this.handleBenClick.bind(this)
+    this.handleInputChange = this.handleInputChange.bind(this)
+    this.handleDelete = this.handleDelete.bind(this)
   }
 
   handleBenClick() {
     this.setState({
       list: [...this.state.list, this.state.inputValue],
-      inputValue : ''
+      inputValue: '',
     })
   }
 
   handleInputChange(e) {
     this.setState({
-      inputValue : e.target.value
+      inputValue: e.target.value,
     })
   }
 
   handleDelete(index) {
-    const list = [...this.state.list];
-    list.splice(index, 1);
+    const list = [...this.state.list]
+    list.splice(index, 1)
     this.setState({ list })
   }
 
   getItems() {
-    return (
-      this.state.list.map((item, index) => {
-        return ( 
-        <InputItem 
-        delete = {this.handleDelete}
-        key = {index}
-        content = {item}
-        index = {index}
-          />
-        )
-        // return <li key={index} onClick={this.handleItemClick.bind(this, index)}>{item}</li>
-      })
-    )
+    return this.state.list.map((item, index) => {
+      return (
+        <InputItem
+          delete={this.handleDelete}
+          key={index}
+          content={item}
+          index={index}
+        />
+      )
+      // return <li key={index} onClick={this.handleItemClick.bind(this, index)}>{item}</li>
+    })
   }
-  
+
   render() {
     return (
       <div>
         <div>
-          <input value = {this.state.inputValue} onChange={this.handleInputChange} />
+          <input
+            value={this.state.inputValue}
+            onChange={this.handleInputChange}
+          />
           <button onClick={this.handleBenClick}>add</button>
         </div>
-        <ul>{ this.getItems() }</ul>
+        <ul>{this.getItems()}</ul>
       </div>
-    );
+    )
   }
 }
 
 // 导出
-export default Inputout;
+export default Inputout
--- helloworld/src/inputItem.js
+++ helloworld/src/inputItem.js
@@ -1,28 +1,24 @@
-import React, { Component } from 'react';
+import React, { Component } from 'react'
 
 class InputItem extends Component {
+  constructor(props) {
+    super(props)
 
-    constructor(props) {
-        super(props);
+    this.handleDelete = this.handleDelete.bind(this)
+  }
 
-        this.handleDelete = this.handleDelete.bind(this);
-    }
+  // 子组件如果想和父组件通信，要调用父组件传递过来的方法
+  handleDelete() {
+    this.props.delete(this.props.index)
+    // console.log(this.props.index);
+  }
 
-    // 子组件如果想和父组件通信，要调用父组件传递过来的方法
-    handleDelete() {
-        this.props.delete(this.props.index);
-        // console.log(this.props.index);
-    }
-
-    // 父组件通过属性的形式向子组件传递参数
-    // 子组件通过props接收父组件传递过来的参数
-    render() {
-        const { content }  = this.props;
-        return (
-            <div onClick={this.handleDelete}>{content}</div>
-        )
-    }
-
+  // 父组件通过属性的形式向子组件传递参数
+  // 子组件通过props接收父组件传递过来的参数
+  render() {
+    const { content } = this.props
+    return <div onClick={this.handleDelete}>{content}</div>
+  }
 }
 
-export default InputItem;
+export default InputItem
