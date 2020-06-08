578c736030a75ed7d59bd0486b079057653baaa2
Passion-Logan <wangdadamailbox@163.com>
2020-06-08 15:22:55 +0800

    添加修改

--- helloworld/src/index.js
+++ helloworld/src/index.js
@@ -2,23 +2,13 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 
 //大写开头的都是组件
-import Inputout from './Inputout'
+// import Inputout from './Inputout'
+// import Clock from './Clock'
+// import Toggle from './Toggle'
+// import LoginControl from './LoginControl'
+import Page from './Page'
 
 // ReactDOM.render(<Inputout />, document.getElementById('root'))
+// ReactDOM.render(<Toggle />, document.getElementById('root'))
 
-function Welcome(props) {
-  return <h1>hello, {props.name}</h1>
-}
-
-// const element = <Welcome name=" world" />
-function App() {
-  return (
-    <div>
-      <Welcome name="aaaa" />
-      <Welcome name="bbb" />
-      <Welcome name="ccc" />
-    </div>
-  )
-}
-
-ReactDOM.render(<App />, document.getElementById('root'))
+ReactDOM.render(<Page />, document.getElementById('root'))
